02070897984d86f2157fcc437fefbdf36b951ac5
datou <1392767180@qq.com>
2019-07-25 14:33:09 +0800

    用expires设置cookie的过期时间

--- src/router/user.js
+++ src/router/user.js
@@ -1,5 +1,10 @@
 const {SuccessModel, ErrorModel} = require('../model/resModel')
 const login = require('../controller/user')
+const getCookieExpires = () => {
+  const d = new Date()
+  d.setTime(d.getTime() + (10 * 24 * 60 * 60 * 1000))
+  return d.toGMTString()
+}
 
 const userRouterHandler = function (req, res) {
   const {username, password} = req.body
@@ -9,7 +14,7 @@ const userRouterHandler = function (req, res) {
     return result.then(data => {
       console.log(data);
       if (data.length !== 0) {
-        res.setHeader('Set-Cookie', `username=${data[0].username}; path=/; httpOnly`)
+        res.setHeader('Set-Cookie', `username=${data[0].username}; path=/; httpOnly; expires=${getCookieExpires()}`)
         return new SuccessModel(data)
       } else {
         return new ErrorModel("登录失败, 请检查用户名或密码是否正确!")
